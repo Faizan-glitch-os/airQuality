8ff5c5cd9b0eb1138daa16b6d2bcd6e616274225
Faizan Ahmad <faizanahmad2582001@gmail.com>
2025-08-10 20:47:12 +0500

    values rounded to 2 values, unit added and Country name showed

--- city.php
+++ city.php
@@ -12,7 +12,6 @@ if (!empty($_GET['city'])) {
     $currentCity = $_GET['city'];
 }
 
-
 //check if currenCity is not empty, then read from cities json and get the filename
 if (!empty($currentCity)) {
     $cities = json_decode(file_get_contents(__DIR__ . '/data/index.json'), true);
@@ -30,20 +29,43 @@ if (!empty($currentCity)) {
 if (!empty($filename)) {
     $results = json_decode(file_get_contents('compress.bzip2://' . __DIR__ . '/data/' . $filename), true)['results'];
 
+    $units = [
+    'pm25' => null,
+    'pm10' => null,
+    ];
+
+    foreach ($results as $result) {
+        if (!empty($units['pm25'] && !empty($units['pm10']))) {
+            break;
+        }
+        if ($result['parameter'] === 'pm25') {
+            $units['pm25'] = $result['unit'];
+        }
+        if ($result['parameter'] === 'pm10') {
+            $units['pm10'] = $result['unit'];
+        }
+    }
+
     $stats = [];
 
     foreach ($results as $result) {
-        if ($result['parameter'] !== 'pm25') {
+        if ($result['parameter'] !== 'pm25' && $result['parameter'] !== 'pm10') {
+            continue;
+        }
+        if ($result['value'] < 0) {
             continue;
         }
 
         $month = substr($result['date']['local'], 0, 7);
 
         if (!isset($stats[$month])) {
-            $stats[$month] = [];
+            $stats[$month] = [
+                'pm25' => [],
+                'pm10' => [],
+            ];
         }
 
-        $stats[$month][] = $result['value'];
+        $stats[$month][$result['parameter']][] = $result['value'];
     }
 }
 
@@ -62,11 +84,24 @@ include_once __DIR__ . '/views/header.inc.php';
 
 <?php else: ?>
     <?php if (!empty($stats)): ?>
+        <h1><?php echo $currentCity ?></h1>
         <table>
+            <thead>
+                <tr>
+                    <th>Month</th>
+                    <th>PM2.5</th>
+                    <th>PM10</th>
+                </tr>
+            </thead>
             <?php foreach ($stats as $month => $measurments): ?>
                 <tr>
                     <th><?php echo e($month); ?></th>
-                    <td><?php echo e(array_sum($measurments) / count($measurments)); ?></td>
+                    <td>
+                        <?php echo round(e(array_sum($measurments['pm25']) / count($measurments['pm25'])), 2); ?> <?php echo $units['pm25'] ?>
+                    </td>
+                    <td>
+                        <?php echo round(e(array_sum($measurments['pm10']) / count($measurments['pm10'])), 2); ?> <?php echo $units['pm10'] ?>
+                    </td>
                 </tr>
             <?php endforeach; ?>
         </table>
